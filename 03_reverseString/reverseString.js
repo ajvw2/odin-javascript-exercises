73dbf86ae733cf541320a4f0d19adda86f9947b9
ajvw2 <129743473+ajvw2@users.noreply.github.com>
2023-06-12 18:57:23 +0200

    Complete exercises 1-4

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,13 @@
-const reverseString = function() {
+const reverseString = function(myString) {
+    // const length = myString.length;
+    // let reversedString = '';
+    
+    // for (let i = 0; i < length; i++) {
+    //     reversedString += myString.slice(-1 - i, length - i);
+    // }
 
+    // return reversedString;
+    return myString.split("").reverse().join("");
 };
 
 // Do not edit below this line
