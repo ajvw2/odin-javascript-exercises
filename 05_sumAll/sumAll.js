bcadf8654097de673e068248fb2a853153872dcd
ajvw2 <129743473+ajvw2@users.noreply.github.com>
2023-06-12 19:28:44 +0200

    Complete exercises 5-7

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,25 @@
-const sumAll = function() {
+const sumAll = function(p, q) {
+    if (p < 0 || q < 0 || typeof p !== "number" || typeof q !== "number") {
+        return "ERROR";
+    }
 
+    let min;
+    let max;
+    if (p < q)
+    {
+        min = p;
+        max = q;
+    } else {
+        min = q;
+        max = p;
+    }
+
+    let totalSum = 0;
+    for (let i = min; i <= max; i++) {
+        totalSum += i;
+    }
+    
+    return totalSum;
 };
 
 // Do not edit below this line
--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,13 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheit) {
+  let celcius = (fahrenheit - 32) * (5 / 9);
+  let celsiusRounded = Math.round(celcius * 10) / 10;
+  return celsiusRounded;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celcius) {
+  let fahrenheit = celcius * (9 / 5) + 32;
+  let fahrenheitRounded = Math.round(fahrenheit * 10) / 10;
+  return fahrenheitRounded;
 };
 
 // Do not edit below this line
